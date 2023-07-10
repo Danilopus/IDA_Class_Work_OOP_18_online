90e3d5e4dbc963da439622891a42dec390832516
Ишутинов Данил <gladius@inbox.ru>
2023-07-10 18:44:00 +0500

    Add project files.

---
+++ IDA_Class_Work_OOP_18_online/!_Main.cpp
@@ -0,0 +1,54 @@
+﻿//class template version 5.0 //07.07.2023//
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <time.h>
+#include <algorithm>
+#include <conio.h>
+//My own headers
+#include "Service functions.h"
+#include "Task_1.h"
+
+
+
+// Class Work 10.07.2023 online -------------------------------------------	
+
+// Умные указатели
+
+//Task 1
+void Task_1()
+{
+	
+}
+//Task 2
+void Task_2()
+{
+	
+}
+//Task 3
+void Task_3()
+{
+
+}
+
+
+// MAIN ------- MAIN ------- MAIN ------- MAIN ------- MAIN --------
+int main()
+{
+srand(time(NULL));
+		
+	
+Task_1();
+Task_2();
+Task_3();
+
+
+
+
+
+std::cout << "\n\n";
+//system("pause");	
+_getch();
+}
+
+
